196e9fb0ad65b3799471adba5258d9382e02b88f
Brandon Washington <BTWashington@student.fullsail.edu>
2020-11-15 15:48:41 -0500

    Added member fields Added Accessors and Mutators Added Pure Virtual Execute() method

--- BaseCommand.h
+++ BaseCommand.h
@@ -1,2 +1,20 @@
+#pragma once
+#include "stdafx.h"
+#include "AbstractState.h"
 
+
+class BaseCommand
+{
+private:
+	// Class members
+	Ship* BaseCommandShip = nullptr;
+	float _timeDelay = 0.0f;
+public:
+	// Accessors and mutators
+	Ship* GetBaseCommandShip() { return BaseCommandShip; }
+	float GetTimeDelay() { return _timeDelay; }
+	// pure virtual Execute method
+	virtual void Execute() {};
+
+};
 
